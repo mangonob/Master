9572bb532738de2750bc9e72cf399e6e6ee670e4
mangonob <534393806@qq.com>
2017-07-11 20:24:30 +0800

    Revert "Remove Asset From ViewController.viewDidLoad()"
    This reverts commit fb2f5314ae3f671fe8a8051efb8c77e45b2703ab.

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -28,6 +28,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        let asset = AVAsset(url: Bundle.main.url(forResource: "video", withExtension: "mp4")!)
+        
+        let mutableComposition = AVMutableComposition()
+        
+        let videoCompositionTrack = mutableComposition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        guard let videoAssetTrack = asset.tracks(withMediaType: AVMediaTypeVideo).first else { return }
+        
+        let allRange = CMTimeRange(start: kCMTimeZero, end: asset.duration)
+        try? videoCompositionTrack.insertTimeRange(allRange, of: videoAssetTrack, at: kCMTimeZero)
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoCompositionTrack)
+        
+        let endTransform = CGAffineTransform.identity.scaledBy(x: 0.49, y: 0.49)
+        layerInstruction.setTransformRamp(fromStart: CGAffineTransform.identity, toEnd: endTransform, timeRange: allRange)
+        
+        instruction.timeRange = allRange
+        instruction.layerInstructions = [layerInstruction]
+        
+        let mutableVideoComposition = AVMutableVideoComposition()
+        mutableVideoComposition.instructions = [instruction]
+        
+        let exportSession = AVAssetExportSession(asset: mutableComposition, presetName: AVAssetExportPresetHighestQuality)
+        exportSession?.videoComposition = mutableVideoComposition
+        exportSession?.outputFileType = AVFileTypeMPEG4
+        let path = "\(NSHomeDirectory())/Documents/\(arc4random()).mp4"
+        print(path)
+        exportSession?.outputURL = URL(fileURLWithPath: path)
+        exportSession?.exportAsynchronously(completionHandler: {
+            print("Completion")
+        })
     }
 }
 
