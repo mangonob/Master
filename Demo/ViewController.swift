5c0f8a6c0e3dbffa17b89104fd53239b60312f56
mangonob <mangonob@qq.com>
2017-07-13 01:33:39 +0800

    Capture

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -28,42 +28,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-        let asset = AVAsset(url: Bundle.main.url(forResource: "video", withExtension: "mp4")!)
-        
-        let mutableComposition = AVMutableComposition()
-        
-        let videoCompositionTrack = mutableComposition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
-        
-        guard let videoAssetTrack = asset.tracks(withMediaType: AVMediaTypeVideo).first else { return }
-        
-        let allRange = CMTimeRange(start: kCMTimeZero, end: asset.duration)
-        try? videoCompositionTrack.insertTimeRange(allRange, of: videoAssetTrack, at: kCMTimeZero)
-        
-        let instruction = AVMutableVideoCompositionInstruction()
-        let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoCompositionTrack)
-        
-        let endTransform = CGAffineTransform.identity.scaledBy(x: 0.49, y: 0.49).translatedBy(x: videoCompositionTrack.naturalSize.width / 2, y: videoCompositionTrack.naturalSize.height / 2)
-        layerInstruction.setTransformRamp(fromStart: CGAffineTransform.identity, toEnd: endTransform,
-                                          timeRange: CMTimeRangeMake(kCMTimeZero, CMTime(seconds: 1, preferredTimescale: 30)))
-        
-        instruction.timeRange = allRange
-        instruction.layerInstructions = [layerInstruction]
-    
-        let mutableVideoComposition = AVMutableVideoComposition()
-        mutableVideoComposition.instructions = [instruction]
-        
-        mutableVideoComposition.renderSize = videoAssetTrack.naturalSize
-        mutableVideoComposition.frameDuration = videoCompositionTrack.minFrameDuration
-        
-        let exportSession = AVAssetExportSession(asset: mutableComposition, presetName: AVAssetExportPresetHighestQuality)
-        exportSession?.videoComposition = mutableVideoComposition
-        exportSession?.outputFileType = AVFileTypeMPEG4
-        let path = "\(NSHomeDirectory())/Documents/\(arc4random()).mp4"
-        print(path)
-        exportSession?.outputURL = URL(fileURLWithPath: path)
-        exportSession?.exportAsynchronously(completionHandler: {
-            print("Completion")
-        })
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSessionPresetHigh
     }
 }
 
