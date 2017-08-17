c65843284ead30a43e161f4e41277736ed14b97a
mangonob <534393806@qq.com>
2017-08-17 20:35:35 +0800

    Create Randomly Varying Dark Scratches

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         
         let sepiaTone = CIFilter(name: "CISepiaTone")
         sepiaTone?.setValue(film, forKey: kCIInputImageKey)
+        sepiaTone?.setValue(NSNumber(value: 1.0), forKey: "inputIntensity")
         
         let randomGenerator = CIFilter(name: "CIRandomGenerator")
         
@@ -36,7 +37,30 @@ class ViewController: UIViewController {
         matrix?.setValue(CIVector(x: 0, y: 1, z: 0, w: 0), forKey: "inputBVector")
         matrix?.setValue(CIVector(x: 0, y: 0, z: 0, w: 0), forKey: "inputBiasVector")
         
-        if let ciImage = matrix?.outputImage {
+        let compositing = CIFilter(name: "CISourceOverCompositing")
+        compositing?.setValue(sepiaTone?.outputImage, forKey: kCIInputImageKey)
+        compositing?.setValue(matrix?.outputImage, forKey: "inputBackgroundImage")
+        
+        let transform = CIFilter(name: "CIAffineTransform")
+        transform?.setValue(randomGenerator?.outputImage, forKey: kCIInputImageKey)
+        transform?.setValue(NSValue(cgAffineTransform: CGAffineTransform(scaleX: 1.5, y: 25)), forKey: kCIInputTransformKey)
+        
+        let matrix2 = CIFilter(name: "CIColorMatrix")
+        matrix2?.setValue(transform?.outputImage, forKey: kCIInputImageKey)
+        matrix2?.setValue(CIVector(x: 4, y: 0, z: 0, w: 0), forKey: "inputRVector")
+        matrix2?.setValue(CIVector(x: 0, y: 0, z: 0, w: 0), forKey: "inputGVector")
+        matrix2?.setValue(CIVector(x: 0, y: 0, z: 0, w: 0), forKey: "inputBVector")
+        matrix2?.setValue(CIVector(x: 0, y: 0, z: 0, w: 0), forKey: "inputAVector")
+        matrix2?.setValue(CIVector(x: 0, y: 1, z: 1, w: 1), forKey: "inputBiasVector")
+        
+        let dark = CIFilter(name: "CIMinimumComponent")
+        dark?.setValue(matrix2?.outputImage, forKey: kCIInputImageKey)
+        
+        let multiply = CIFilter(name: "CIMultiplyCompositing")
+        multiply?.setValue(dark?.outputImage, forKey: kCIInputImageKey)
+        multiply?.setValue(compositing?.outputImage, forKey: "inputBackgroundImage")
+        
+        if let ciImage = multiply?.outputImage {
             if let cgImage = context.createCGImage(ciImage, from: film.extent) {
                 imageView.image = UIImage(cgImage: cgImage)
             }
