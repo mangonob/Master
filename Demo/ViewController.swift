360be9e3f6691d9cf5ec34a310d9bcc82d86f1b8
mangonob <534393806@qq.com>
2017-08-17 19:44:07 +0800

    +

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -18,37 +18,26 @@ import ChameleonFramework
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
-    let from = CIImage(contentsOf: Bundle.main.url(forResource: "from", withExtension: "png")!)!
-    let to = CIImage(contentsOf: Bundle.main.url(forResource: "to", withExtension: "png")!)!
-    
-    var filter: CIFilter!
-    var pixellate: CIFilter!
-    
-    lazy var context = CIContext()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        filter = CIFilter(name: "CIDissolveTransition")!
-        filter.setValue(from, forKey: kCIInputImageKey)
-        filter.setValue(to, forKey: "inputTargetImage")
+        let film = CIImage(contentsOf: Bundle.main.url(forResource: "film", withExtension: "png")!)!
+        let context = CIContext()
         
-        pixellate = CIFilter(name: "CIPixellate")
+        let sepiaTone = CIFilter(name: "CISepiaTone")
+        sepiaTone?.setValue(film, forKey: kCIInputImageKey)
         
-        sliderAction(sender: nil)
-    }
-    
-    @IBAction func sliderAction(sender: UISlider?) {
-        let x = sender?.value ?? 0.0
-        let y = 1 - 2 * abs(x - 0.5)
+        let randomGenerator = CIFilter(name: "CIRandomGenerator")
         
-        filter.setValue(NSNumber(value: x), forKey: "inputTime")
+        let matrix = CIFilter(name: "CIColorMatrix")
+        matrix?.setValue(randomGenerator?.outputImage, forKey: kCIInputImageKey)
+        matrix?.setValue(CIVector(x: 0, y: 1, z: 0, w: 0), forKey: "inputRVector")
+        matrix?.setValue(CIVector(x: 0, y: 1, z: 0, w: 0), forKey: "inputGVector")
+        matrix?.setValue(CIVector(x: 0, y: 1, z: 0, w: 0), forKey: "inputBVector")
+        matrix?.setValue(CIVector(x: 0, y: 0, z: 0, w: 0), forKey: "inputBiasVector")
         
-        pixellate.setValue(filter.outputImage, forKey: kCIInputImageKey)
-        pixellate.setValue(NSNumber(value: y * 90 + 0.1), forKey: "inputScale")
-        
-        if let ciImage = pixellate.outputImage {
-            if let cgImage = context.createCGImage(ciImage, from: .init(origin: .zero, size: .init(width: 391, height: 300))) {
+        if let ciImage = matrix?.outputImage {
+            if let cgImage = context.createCGImage(ciImage, from: film.extent) {
                 imageView.image = UIImage(cgImage: cgImage)
             }
         }
