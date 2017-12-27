b8153af2f035307083b573f5c3b973b3ed172b31
mangonob <mangonob@qq.com>
2017-12-27 16:49:08 +0800

    *

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,64 +9,154 @@
 import UIKit
 import CoreText
 import MBProgressHUD
+import ChameleonFramework
 
-
-/// 定义 `protocol`
-public protocol SelfAware: class {
-    static func awake()
-}
-
-// 创建代理执行单例
-class NothingToSeeHere{
+class Cell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
     
-    static func harmlessFunction(){
-        let typeCount = Int(objc_getClassList(nil, 0))
-        let  types = UnsafeMutablePointer<AnyClass?>.allocate(capacity: typeCount)
-        let autoreleaseintTypes = AutoreleasingUnsafeMutablePointer<AnyClass?>(types)
-        objc_getClassList(autoreleaseintTypes, Int32(typeCount)) //获取所有的类
-        for index in 0 ..< typeCount{
-            (types[index] as? SelfAware.Type)?.awake() //如果该类实现了SelfAware协议，那么调用awake方法
+    var number: NSNumber? {
+        didSet {
+            if let number = number {
+                label.text = number.description
+            }
         }
-        types.deallocate(capacity: typeCount)
     }
 }
 
-/// 执行单例
-extension UIApplication {
-    public static let runOnce:Void = {
-        //使用静态属性以保证只调用一次(该属性是个方法)
-        NothingToSeeHere.harmlessFunction()
-    }()
-    
-    open override var next: UIResponder?{
-        UIApplication.runOnce
-        return super.next
-    }
-}
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.collectionViewLayout = Layout()
+        var transform = CATransform3DIdentity
+        transform.m34 = -1/500.0
+        collectionView.layer.sublayerTransform = transform
+    }
+    
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: nil)
+        if let layout = collectionView.collectionViewLayout as? Layout {
+            layout.progress = min(max(location.x / collectionView.bounds.width, 0), 1)
+        }
+    }
+    
+    @IBAction func tapAction(_ sender: UITapGestureRecognizer) {
+        let layout = Layout()
+        layout.progress = 0.5
+        
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        super.motionBegan(motion, with: event)
     }
 }
 
 
-extension UIViewController: SelfAware {
-    private static let awakeOnce: Void = {
-        let defaultImp = class_getInstanceMethod(UIViewController.self, #selector(UIViewController.viewDidLoad))
-        
-        let xxxImp = class_getInstanceMethod(UIViewController.self, #selector(UIViewController.xxx_viewDidLoad))
-        
-        method_exchangeImplementations(defaultImp, xxxImp)
-    }()
-    
-    public static func awake() {
-        awakeOnce
-    }
-    
-    func xxx_viewDidLoad() {
-        self.xxx_viewDidLoad()
-        
-        print("\(self) Did Load!")
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
     }
 }
+
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        cell.number = NSNumber(value: indexPath.row)
+        cell.backgroundColor = UIColor.clear
+        cell.contentView.layer.cornerRadius = 10
+        cell.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.shadowRadius = 5
+        cell.layer.shadowOffset = .zero
+        cell.contentView.backgroundColor = UIColor.randomFlat
+        return cell
+    }
+}
+
+@objc protocol LayoutDelegate: UICollectionViewDelegate {
+    @objc optional func collectionView(_ collectionView: UICollectionView, layout: Layout, itemSizeAt indexPath: IndexPath) -> CGSize
+    
+    @objc optional func collectionView(_ collectionView: UICollectionView, radiusOfLayout: Layout) -> CGFloat
+}
+
+extension ViewController: LayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout: Layout, itemSizeAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+}
+
+class Layout: UICollectionViewLayout {
+    override var collectionViewContentSize: CGSize {
+        return collectionView?.bounds.size ?? .zero
+    }
+    
+    var itemSize: CGSize = .zero {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    var progress: CGFloat = 0 {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    var radius: CGFloat = 100 {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let collectionView = collectionView else { return nil }
+        
+        var attributes = [UICollectionViewLayoutAttributes]()
+        
+        let radius = (collectionView.delegate as? LayoutDelegate)?.collectionView?(collectionView, radiusOfLayout: self) ?? self.radius
+        
+        for section in 0..<collectionView.numberOfSections {
+            let numberOfItems = CGFloat(collectionView.numberOfItems(inSection: section))
+            
+            var currentProgress = progress
+            let progressStep = numberOfItems > 0 ? 1 / numberOfItems : 0
+            
+            for row in 0..<collectionView.numberOfItems(inSection: section) {
+                defer {
+                    currentProgress += progressStep
+                }
+                
+                let indexPath = IndexPath(row: row, section: section)
+                let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+                attribute.center = CGPoint(x: collectionView.bounds.midX, y: collectionView.bounds.midY)
+                attribute.bounds = CGRect(origin: .zero, size: (collectionView.delegate as? LayoutDelegate)?.collectionView?(collectionView, layout: self, itemSizeAt: indexPath) ?? itemSize)
+                
+                var transform = CATransform3DIdentity
+                transform = CATransform3DTranslate(transform, radius * sin(currentProgress * CGFloat.pi * 2), 0, radius * cos(currentProgress * CGFloat.pi * 2))
+                
+                attribute.transform3D = transform
+                attribute.zIndex = -row
+                
+                attributes.append(attribute)
+            }
+        }
+        
+        return attributes
+    }
+}
+
+
