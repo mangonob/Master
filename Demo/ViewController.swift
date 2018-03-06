902a7d1a41d8ca06d5daa11a78b6f1603110d5a1
mangonob <mangonob@qq.com>
2018-03-06 17:40:16 +0800

    Gray Triangle

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -8,27 +8,71 @@
 
 import UIKit
 import Metal
+import simd
+import MetalKit
 
 class ViewController: UIViewController {
-
+    var metalLayer: CAMetalLayer? {
+        return view.layer as? CAMetalLayer
+    }
+    
+    var pipelineState: MTLRenderPipelineState!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
+        UIApplication.shared.isStatusBarHidden = true
+        
         if let device = MTLCreateSystemDefaultDevice(),
             let queue = device.makeCommandQueue(),
-            let commandBuffer = queue.makeCommandBuffer() {
-            let library = device.makeDefaultLibrary()
-            let fun = library?.makeFunction(name: "func")
+            let commandBuffer = queue.makeCommandBuffer(),
+            let library = device.makeDefaultLibrary() {
+
+            var vertexBuffer: MTLBuffer! = nil
+            let vertexData: [Float] = [
+                0.0, 1.0, 0.0,
+                -1.0, -1.0, 0.0,
+                1.0, -1.0, 0.0
+            ]
             
+            vertexBuffer = device.makeBuffer(bytes: vertexData, length: vertexData.count * MemoryLayout<Float>.size, options: [])
+            
+            let defaultLibrary = device.makeDefaultLibrary()
+            let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
+            let vertexProgram = defaultLibrary?.makeFunction(name: "basic_vertex")
+            let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+            pipelineStateDescriptor.vertexFunction = vertexProgram
+            pipelineStateDescriptor.fragmentFunction = fragmentProgram
+            pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+            pipelineState = try? device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+            
+            metalLayer?.device = device
+            
+            if let drawable = metalLayer?.nextDrawable() {
+                let renderPassDescriptor = MTLRenderPassDescriptor()
+                renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+                renderPassDescriptor.colorAttachments[0].loadAction = .clear
+                renderPassDescriptor.colorAttachments[0].clearColor = .init(red: 0, green: 1, blue: 0, alpha: 1)
+                
+                let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+                encoder?.setRenderPipelineState(pipelineState)
+                encoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+                encoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+                encoder?.endEncoding()
+                
+                commandBuffer.present(drawable)
+                commandBuffer.commit()
+            }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
