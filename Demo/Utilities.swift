3cd04b6b3f3c1036848888d695a2eb856fface24
mangonob <mangonob@qq.com>
2018-04-10 17:34:11 +0800

    + Utilities.swift

---
+++ Demo/Utilities.swift
@@ -0,0 +1,57 @@
+//
+//  Utilities.swift
+//  Demo
+//
+//  Created by 高炼 on 2018/4/10.
+//  Copyright © 2018年 高炼. All rights reserved.
+//
+
+import Foundation
+import simd
+
+
+/// Make a rotation transform.
+func matrix4x4_rotation(radians: Float, axis: float3) -> matrix_float4x4 {
+    let unitAxis = normalize(axis)
+    let ct = cosf(radians)
+    let st = sinf(radians)
+    let ci = 1 - ct
+    let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
+    return matrix_float4x4.init(columns:(vector_float4(    ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0),
+                                         vector_float4(x * y * ci - z * st,     ct + y * y * ci, z * y * ci + x * st, 0),
+                                         vector_float4(x * z * ci + y * st, y * z * ci - x * st,     ct + z * z * ci, 0),
+                                         vector_float4(                  0,                   0,                   0, 1)))
+}
+
+/// Make a translation transform.
+func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> matrix_float4x4 {
+    return matrix_float4x4.init(columns:(vector_float4(1, 0, 0, 0),
+                                         vector_float4(0, 1, 0, 0),
+                                         vector_float4(0, 0, 1, 0),
+                                         vector_float4(translationX, translationY, translationZ, 1)))
+}
+
+
+/// Make a scale transform.
+func matrix4x4_scale(_ x: Float, _ y: Float, _ z: Float) -> matrix_float4x4 {
+    return matrix_float4x4.init(columns: (vector_float4(x, 0, 0, 0),
+                                          vector_float4(0, y, 0, 0),
+                                          vector_float4(0, 0, z, 0),
+                                          vector_float4(0, 0, 0, 1)))
+}
+
+
+/// Make a perspective projection transform in right hand coordination.
+func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> matrix_float4x4 {
+    let ys = 1 / tanf(fovy * 0.5)
+    let xs = ys / aspectRatio
+    let zs = farZ / (nearZ - farZ)
+    return matrix_float4x4.init(columns:(vector_float4(xs,  0, 0,   0),
+                                         vector_float4( 0, ys, 0,   0),
+                                         vector_float4( 0,  0, zs, -1),
+                                         vector_float4( 0,  0, zs * nearZ, 0)))
+}
+
+func radians_from_degrees(_ degrees: Float) -> Float {
+    return (degrees / 180) * .pi
+}
