b8f95db67b7007db81fe80bd88bf6579c683ea37
mangonob <534393806@qq.com>
2018-04-19 15:30:14 +0800

    + Wraith raider startship

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -34,6 +34,21 @@ class ViewController: UIViewController {
         mtkView.delegate = render
         
         render.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+        
+        ///
+        let modelBundle = Bundle.init(url: Bundle.main.url(forResource: "Model", withExtension: "bundle")!)!
+        let asset = MDLAsset.init(url: modelBundle.url(forResource: "Temple", withExtension: "obj")!)
+        let mesh = asset.object(at: 0) as! MDLMesh
+        let submeshes = mesh.submeshes as! [MDLSubmesh]
+        submeshes.forEach { (submesh) in
+            if #available(iOS 11.0, *) {
+                submesh.material?.properties(with: .ambientOcclusion).forEach({ (property) in
+                    print(property.urlValue)
+                })
+            } else {
+                // Fallback on earlier versions
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
